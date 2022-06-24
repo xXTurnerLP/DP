44268bf9c66b767ab8eac1d11c66ac1d147bc917
xXTurnerLP <xxturnerbgdani@gmail.com>
2022-06-24 22:17:05 +0300

    Added base controllers & basic model

---
+++ DP/Models/Property.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DP.Models
+{
+	public class Property
+	{
+		public string Type { get; set; } // eg. "house", "apartment", "store"
+		public string State { get; set; } // "sold", "available" or "reserved"
+		public bool PriceHidden { get; set; }
+		public double Price { get; set; }
+	}
+}
