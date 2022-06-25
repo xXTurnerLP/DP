438df9d01616a58b377d0239afe4fe494610d1ba
xXTurnerLP <xxturnerbgdani@gmail.com>
2022-06-25 15:31:42 +0300

    added internal admin page

---
+++ DP/Controllers/InternalAdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DP.Controllers
+{
+	public class InternalAdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View("PasswordProtected");
+		}
+
+		public IActionResult Authorize()
+		{
+			return View();
+		}
+	}
+}
