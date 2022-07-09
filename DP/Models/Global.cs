df82dbf5a7757b05ca95423c3bea02625689c44a
xXTurnerLP <xxturnerbgdani@gmail.com>
2022-07-09 18:34:58 +0300

    edit index of about

---
+++ DP/Models/Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.Models
+{
+    public class Global
+    {
+		public int Id { get; set; }
+		public string AboutText { get; set; }
+		public string ContactsAddress { get; set; }
+		public string ContactsPhone { get; set; }
+		public string ContactsEmail { get; set; }
+	}
+}
